da37f9b75701a4a5882c12d51276230a24f95d0a
Ramesh Gehlot <2024aa05599@wilp.bits-pilani.ac.in>
2026-02-02 18:30:28 +0530

    create main app file

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import time
+# from retriever import dense_retrieve, sparse_retrieve, rrf, corpus
+# from generator import generate_answer
+
+st.title("Hybrid RAG System")
+
+query = st.text_input("Enter your question")
+
+if query:
+    start = time.time()
+
+    # dense = dense_retrieve(query)
+    # sparse = sparse_retrieve(query)
+    # fused = rrf(dense, sparse)[:5]
+
+    # contexts = [corpus[i]["text"] for i,_ in fused]
+    # answer = generate_answer(query, contexts)
+
+    # st.subheader("Answer")
+    # st.write(answer)
+
+    # st.subheader("Retrieved Chunks")
+    # for i,score in fused:
+    #     with st.expander(corpus[i]["title"]):
+    #         st.write(corpus[i]["text"])s
+    #         st.write("Source:", corpus[i]["url"])
+    #         st.write("RRF Score:", score)
+
+    st.write("Response Time:", round(time.time()-start,2), "seconds")
